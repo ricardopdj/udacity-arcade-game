fd4f2d826d84bd2c767209f16696cda37bdf2073
Ricardo <pareja.r@gmail.com>
2018-09-14 23:20:12 -0300

    basic game working

--- js/app.js
+++ js/app.js
@@ -1,38 +1,40 @@
-class Enemy {
-    constructor(y) {
-        this.sprite = 'images/enemy-bug.png';
-        this.speed = Math.floor(Math.random() * 300) + 50;
-        this.x = -170;
+class Char {
+    constructor(x, y, sprite) {
+        this.x = x;
         this.y = y;
+        this.sprite = sprite;
     }
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+}
+
+class Enemy extends Char {
+    constructor(x = -170, y, sprite = 'images/enemy-bug.png') {
+        super(x, y, sprite);
+        this.speed = Math.floor(Math.random() * 300) + 50;
+    }
 
     update(dt) {
         this.x = this.x + this.speed * dt;
+        if (this.x >= 505) {
+            this.x = -170;
+        }
         if (player.x + 70 > this.x && player.x < this.x + 100 && player.y + 85 > this.y && player.y < this.y + 70) {
             player.crashed();
         }
     }
 }
 
-class Player {
-    constructor() {
-        this.x = 200;
-        this.y = 390;
-        this.sprite = 'images/char-boy.png';
-    }
-
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+class Player extends Char {
+    constructor(x = 200, y = 390, sprite = 'images/char-boy.png') {
+        super(x, y, sprite);
     }
 
     update() {
         if (this.y == -35) {
-            $message.style.display = "block";
-            clearInterval(teste);
+            game.finish();
         }
     }
 
@@ -71,36 +73,36 @@ class Player {
     }
 }
 
-// Game variables;
-let player = new Player(),
-    allEnemies = new Array(),
-    enemiesRows = [60, 140, 225],
-    allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-let teste;
-let $message = document.getElementById("message");
-
 class Game {
     constructor() {
+        this.handleMoves();
+        this.addEnemies();
+    }
+    handleMoves() {
         document.addEventListener('keyup', function (e) {
             player.handleInput(allowedKeys[e.keyCode]);
         });
-        this.addEnemies();
-        teste = setInterval(
-            this.addEnemies.bind(this),
-            2500
-        )
     }
     addEnemies() {
         for (let index = 0; index < 3; index++) {
             let y = enemiesRows[index];
-            allEnemies.push(new Enemy(y));
+            allEnemies.push(new Enemy(-170, y));
         }
+    }
+    finish() {
+        message.style.display = 'block';
+        allEnemies = new Array();
     }
 }
 
-new Game();
+let message = document.getElementById('message');
+let allEnemies = new Array(),
+enemiesRows = [60, 140, 225],
+allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
+const game = new Game();
+const player = new Player();
